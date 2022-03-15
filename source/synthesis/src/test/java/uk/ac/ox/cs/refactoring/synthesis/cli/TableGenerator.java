a76f83259121cca0e1893b6e44147ea49754d070
Pascal Kesseli <pascal_kesseli@hotmail.com>
2022-03-15 01:23:39 +0100

    More summaries
    Add more summaries to table generator.

--- source/synthesis/src/test/java/uk/ac/ox/cs/refactoring/synthesis/cli/TableGenerator.java
+++ source/synthesis/src/test/java/uk/ac/ox/cs/refactoring/synthesis/cli/TableGenerator.java
@@ -18,6 +18,7 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -28,11 +29,14 @@ import uk.ac.ox.cs.refactoring.synthesis.statistics.Run;
 
 public class TableGenerator {
 
+  private static final int NUMBER_OF_SUMMARY_COLUMNS = 4;
+
   public static void main(final String[] args) throws IOException {
     final String[] files = Arrays.stream(args).map(Paths::get).filter(Files::exists).map(Path::toString)
         .toArray(String[]::new);
     final int numberOfConfigurations = files.length;
     final List<List<String>> cells = getCells(files);
+    final int numberOfBenchmarks = cells.size() - NUMBER_OF_SUMMARY_COLUMNS - 1;
     try (final OutputStream os = Files.newOutputStream(Paths.get("table.tex"));
         final OutputStream buffer = new BufferedOutputStream(os);
         final PrintWriter writer = new PrintWriter(buffer)) {
@@ -43,6 +47,9 @@ public class TableGenerator {
       writer.println("\\usepackage{wasysym}");
       writer.println("\\newcommand{\\xmark}{\\ding{55}}");
       writer.println("\\begin{document}");
+
+      writer.print("Number of benchmarks: ");
+      writer.println(numberOfBenchmarks);
 
       writer.print("\\begin{longtable}{ l ");
       for (int i = 0; i < numberOfConfigurations; ++i)
@@ -56,7 +63,7 @@ public class TableGenerator {
 
         if (rowIndex == 0)
           writer.println("\\hline");
-        if (rowIndex == cells.size() - 2) {
+        if (rowIndex == cells.size() - NUMBER_OF_SUMMARY_COLUMNS - 1) {
           writer.println("\\hline");
           writer.println("\\hline");
         }
@@ -65,6 +72,10 @@ public class TableGenerator {
       writer.println("\\end{document}");
     }
   }
+
+  private static final String UNSOUND_MARKER = "\\multicolumn{1}{c}{\\lightning}";
+
+  private static final String MISSED_MARKER = "\\multicolumn{1}{c}{\\xmark}";
 
   private static List<List<String>> getCells(final String[] jsonFiles) throws IOException {
     if (jsonFiles.length == 0)
@@ -85,7 +96,7 @@ public class TableGenerator {
       reports.put(jsonFiles[column], report);
 
       final int numberOfBenchmarks = report.Benchmarks.size();
-      numberOfRows = numberOfBenchmarks + 2;
+      numberOfRows = numberOfBenchmarks + NUMBER_OF_SUMMARY_COLUMNS + 1;
       if (cells.isEmpty()) {
         for (int i = 0; i < numberOfRows; ++i)
           cells.add(new ArrayList<>(Collections.nCopies(numberOfColumns, null)));
@@ -113,13 +124,13 @@ public class TableGenerator {
         final List<Run> runs = benchmark.getValue();
         final boolean isUnsound = runs.stream().anyMatch(run -> run.Unsound);
         if (isUnsound) {
-          row.set(columnIndex, "\\multicolumn{1}{c}{\\lightning}");
+          row.set(columnIndex, UNSOUND_MARKER);
           continue;
         }
 
         final boolean hasNoSolution = runs.stream().anyMatch(run -> run.Solution == null);
         if (hasNoSolution) {
-          row.set(columnIndex, "\\multicolumn{1}{c}{\\xmark}");
+          row.set(columnIndex, MISSED_MARKER);
           continue;
         }
 
@@ -129,14 +140,33 @@ public class TableGenerator {
       }
     }
 
-    cells.get(numberOfRows - 1).set(0, "Average");
+    final int averageRow = numberOfRows - NUMBER_OF_SUMMARY_COLUMNS;
+    final int soundRow = averageRow + 1;
+    final int unsoundRow = soundRow + 1;
+    final int missedRow = unsoundRow + 1;
+
+    cells.get(averageRow).set(0, "Average");
+    cells.get(soundRow).set(0, "Sound");
+    cells.get(unsoundRow).set(0, "Unsound");
+    cells.get(missedRow).set(0, "Missed");
+
     for (int column = 1; column < numberOfConfigurations + 1; ++column) {
-      final int columnIndex = column;
-      final long average = Math.round(cells.stream().skip(1).map(row -> row.get(columnIndex))
+      final long average = Math.round(getRow(cells, column)
           .filter(NumberUtils::isParsable).collect(Collectors.averagingLong(Long::parseLong)));
-      cells.get(numberOfRows - 1).set(column, Long.toString(average));
+      final long sound = getRow(cells, column).filter(NumberUtils::isParsable).count();
+      final long unsound = getRow(cells, column).filter(UNSOUND_MARKER::equals).count();
+      final long missed = getRow(cells, column).filter(MISSED_MARKER::equals).count();
+
+      cells.get(averageRow).set(column, Long.toString(average));
+      cells.get(soundRow).set(column, Long.toString(sound));
+      cells.get(unsoundRow).set(column, Long.toString(unsound));
+      cells.get(missedRow).set(column, Long.toString(missed));
     }
 
     return cells;
+  }
+
+  private static Stream<String> getRow(final List<List<String>> cells, final int column) {
+    return cells.stream().skip(1).map(row -> row.get(column));
   }
 }
