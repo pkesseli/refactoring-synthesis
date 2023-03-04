4532f8e99dfe620d21876706255354e0f4e19592
Hanliang <hanliangzhang.cm@gmail.com>
2023-03-04 15:23:02 +0000

    add generated queries

--- chatgpt/javadoc-scraper/src/main.rs
+++ chatgpt/javadoc-scraper/src/main.rs
@@ -1,7 +1,17 @@
+use clap::Parser;
 use std::path::PathBuf;
-use std::str::FromStr;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long)]
+    javadoc: bool,
+    #[arg(long, default_value_t = String::from("queries"))]
+    output_dir: String,
+}
 
 fn main() {
+    let args = Args::parse();
+
     let response = reqwest::blocking::get(
         "https://docs.oracle.com/en/java/javase/15/docs/api/deprecated-list.html",
     )
@@ -11,31 +21,39 @@ fn main() {
 
     let document = scraper::Html::parse_document(&response);
 
-    let method_selector = scraper::Selector::parse("div#method table.summary-table tbody tr").unwrap();
+    let method_selector =
+        scraper::Selector::parse("div#method table.summary-table tbody tr").unwrap();
 
     let deprecated_methods = document.select(&method_selector);
 
     let name_selector = scraper::Selector::parse("a").unwrap();
     let comment_selector = scraper::Selector::parse(".deprecation-comment").unwrap();
 
-    let queries_dir = PathBuf::from_str("queries").unwrap();
+    let queries_dir = PathBuf::from(args.output_dir);
     if !queries_dir.is_dir() {
         std::fs::create_dir_all(&queries_dir).unwrap();
     }
 
     for (element, idx) in deprecated_methods.zip(0..) {
         let method_name = element.select(&name_selector).next().unwrap().inner_html();
-        let deprecation_comment = element.select(&comment_selector).next()
+        let deprecation_comment = element
+            .select(&comment_selector)
+            .next()
             .map(|element| {
                 let mut comment = "".to_owned();
                 for text in element.text() {
                     comment += text;
                 }
                 comment
-            }).unwrap_or_else(|| "".to_owned());
+            })
+            .unwrap_or_else(|| "".to_owned());
 
         let mut query = format!("The method {method_name} is deprecated.");
-        query += &format!(" The related deprecation comment in Javadoc is \"{deprecation_comment}\"\n");
+        if args.javadoc {
+            query += &format!(
+                " The related deprecation comment in Javadoc is \"{deprecation_comment}\"\n"
+            );
+        }
         query += "Show me a refactoring example.";
 
         let query_dir = queries_dir.join(&idx.to_string());
--- chatgpt/query.py
+++ chatgpt/query.py
@@ -1,29 +1,46 @@
 import os
 import sys
-from revChatGPT.V1 import Chatbot
+from revChatGPT.V1 import Chatbot, Error
+from time import time, sleep
+import itertools
 
-chatbot = Chatbot(config={
-    "email": input("Email: "),
-    "password": input("Password: ")
-})
+# chatbot = Chatbot(config={
+#     "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJwZDIxNTQxQGJyaXN0b2wuYWMudWsiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZ2VvaXBfY291bnRyeSI6IkdCIn0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1JdERMY2U3ZHFSMDR5c3NvWHo0WDlxVlkifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6ImF1dGgwfDY0MDFlN2E0ZmE3NjY5ZDI0YzBiYjM4MiIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkub3BlbmFpLmF1dGgwYXBwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2Nzc4NDY0OTAsImV4cCI6MTY3OTA1NjA5MCwiYXpwIjoiVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG1vZGVsLnJlYWQgbW9kZWwucmVxdWVzdCBvcmdhbml6YXRpb24ucmVhZCBvZmZsaW5lX2FjY2VzcyJ9.1YPOAiD6UekoubJuPd3opWpTnWcSQ5LY8-5edvsAcyynXZ2sAXJzBSAm_c6FsK1siQ6jc70zmgW_FJFuPwQy7ovv02Jd1n3TYEvZm1BN21BlW6_PBPrZx9EePVQyOIwjqHE2ZjQTjX8EiTTXZj5y14AG4ZkQWE7XiTeYq6VYfgHT4G9GQm-r5CWb9MRb9FX0yjUu9b5JWszUsnFhVLYiPCNZ5u2YMqaHw8MmZizsxy0Tr9tZAESxkRc9KXdViphcu5GXMcHSpZPhhYs0eqqGt8sDLEqsJ8araNRxF2Yj78aA7-yekAQAdG2O1q80KmnnxFL5ZslNNLOs9WSjmgZhTQ"
+# })
 
+queries_dir = sys.argv[1]
 
-entries = os.listdir('queries')
+# entries = os.listdir(queries_dir)
 
-for entry in entries:
-    entry = os.path.join('queries', entry)
+for entry in range(sys.maxsize):
+    entry = str(entry)
+    entry = os.path.join(queries_dir, entry)
+    if not os.path.exists(entry):
+        break
+    print('Querying ' + os.path.basename(entry))
+    chatbot = Chatbot(config={
+        "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJwZDIxNTQxQGJyaXN0b2wuYWMudWsiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZ2VvaXBfY291bnRyeSI6IkdCIn0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1JdERMY2U3ZHFSMDR5c3NvWHo0WDlxVlkifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6ImF1dGgwfDY0MDFlN2E0ZmE3NjY5ZDI0YzBiYjM4MiIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkub3BlbmFpLmF1dGgwYXBwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2Nzc4NDY0OTAsImV4cCI6MTY3OTA1NjA5MCwiYXpwIjoiVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG1vZGVsLnJlYWQgbW9kZWwucmVxdWVzdCBvcmdhbml6YXRpb24ucmVhZCBvZmZsaW5lX2FjY2VzcyJ9.1YPOAiD6UekoubJuPd3opWpTnWcSQ5LY8-5edvsAcyynXZ2sAXJzBSAm_c6FsK1siQ6jc70zmgW_FJFuPwQy7ovv02Jd1n3TYEvZm1BN21BlW6_PBPrZx9EePVQyOIwjqHE2ZjQTjX8EiTTXZj5y14AG4ZkQWE7XiTeYq6VYfgHT4G9GQm-r5CWb9MRb9FX0yjUu9b5JWszUsnFhVLYiPCNZ5u2YMqaHw8MmZizsxy0Tr9tZAESxkRc9KXdViphcu5GXMcHSpZPhhYs0eqqGt8sDLEqsJ8araNRxF2Yj78aA7-yekAQAdG2O1q80KmnnxFL5ZslNNLOs9WSjmgZhTQ"
+    })
     with open(os.path.join(entry, 'query.txt'), 'r') as file:
         query = file.read()
 
-    original_stdout = sys.stdout
     with open(os.path.join(entry, 'response.txt'), 'w') as file:
-        sys.stdout = file
-        prev_text = ""
-        for data in chatbot.ask(
-            query,
-        ):
-            message = data["message"][len(prev_text) :]
-            print(message, end="", flush=True)
-            prev_text = data["message"]
-        print()
-        sys.stdout = original_stdout
+        while True:
+            try:
+                start = time()
+                response = ""
+                for data in chatbot.ask(
+                    query,
+                ):
+                    response = data["message"]
+                elapsed = time() - start
+                file.write('# Response Time: ' + str(elapsed))
+                file.write(response)
+                # chatbot.rollback_conversation()
+            except Error as e:
+                if e.code != 2:
+                    raise
+                else:
+                    print('Rate limit exceeded, waiting for one hour..')
+                    sleep(3600)
+                    continue
