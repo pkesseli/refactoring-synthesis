b508ceb32eb94534623ae6413b00383f6d2903f9
Pascal Kesseli <pascal_kesseli@hotmail.com>
2023-03-25 17:14:58 +0100

    Keep experiment logs
    Keep experiment logs for each run.

--- experiment.sh
+++ experiment.sh
@@ -7,21 +7,21 @@ maxCounterexamples=1
 # Single example test run
 # mvn --file source/pom.xml --define resynth.synthesis.javadoc=true --define resynth.fuzzing.random=false --define resynth.verification.stage1.maxInputs=$maxInputs --define resynth.verification.stage1.maxCounterexamples=$maxCounterexamples --define resynth.verification.stage2.maxInputs=0 --define resynth.verification.stage2.maxCounterexamples=0 --define resynth.seed=$seed --define failIfNoTests=false --define trimStackTrace=false --define test=java_awt_TextFieldTest#minimumSize clean test >zest-javadoc.log 2>&1
 # mvn --file source/pom.xml --projects='!instrument' --also-make --define exec.classpathScope=test --define exec.mainClass=uk.ac.ox.cs.refactoring.synthesis.cli.Experiment compile exec:java
-# mv source/synthesis/target/surefire-reports/summary.json source/synthesis/target/surefire-reports/zest-javadoc.json
+# mv source/synthesis/target/surefire-reports/summary.json source/synthesis/zest-javadoc.json
 # exit 0
 
 mvn --file source/pom.xml --define resynth.synthesis.javadoc=false --define resynth.fuzzing.random=false --define resynth.verification.stage1.maxInputs=$maxInputs --define resynth.verification.stage1.maxCounterexamples=$maxCounterexamples --define resynth.verification.stage2.maxInputs=0 --define resynth.verification.stage2.maxCounterexamples=0 --define resynth.seed=$seed --define failIfNoTests=false --define trimStackTrace=false --define test="uk.ac.ox.cs.refactoring.synthesis.experiment.**" clean test >zest.log 2>&1
 mvn --file source/pom.xml --projects='!instrument' --also-make --define exec.classpathScope=test --define exec.mainClass=uk.ac.ox.cs.refactoring.synthesis.cli.Experiment compile exec:java
-mv source/synthesis/target/surefire-reports/summary.json source/synthesis/target/surefire-reports/zest.json
+cp -r source/synthesis/target/surefire-reports source/synthesis/results-zest
 
 mvn --file source/pom.xml --define resynth.synthesis.javadoc=true --define resynth.fuzzing.random=false --define resynth.verification.stage1.maxInputs=$maxInputs --define resynth.verification.stage1.maxCounterexamples=$maxCounterexamples --define resynth.verification.stage2.maxInputs=0 --define resynth.verification.stage2.maxCounterexamples=0 --define resynth.seed=$seed --define failIfNoTests=false --define trimStackTrace=false --define test="uk.ac.ox.cs.refactoring.synthesis.experiment.**" clean test >zest-javadoc.log 2>&1
 mvn --file source/pom.xml --projects='!instrument' --also-make --define exec.classpathScope=test --define exec.mainClass=uk.ac.ox.cs.refactoring.synthesis.cli.Experiment compile exec:java
-mv source/synthesis/target/surefire-reports/summary.json source/synthesis/target/surefire-reports/zest-javadoc.json
+cp -r source/synthesis/target/surefire-reports source/synthesis/results-zest-javadoc
 
 mvn --file source/pom.xml --define resynth.synthesis.javadoc=false --define resynth.fuzzing.random=true --define resynth.verification.stage1.maxInputs=$maxInputs --define resynth.verification.stage1.maxCounterexamples=$maxCounterexamples --define resynth.verification.stage2.maxInputs=0 --define resynth.verification.stage2.maxCounterexamples=0 --define resynth.seed=$seed --define failIfNoTests=false --define trimStackTrace=false --define test="uk.ac.ox.cs.refactoring.synthesis.experiment.**" clean test >random.log 2>&1
 mvn --file source/pom.xml --projects='!instrument' --also-make --define exec.classpathScope=test --define exec.mainClass=uk.ac.ox.cs.refactoring.synthesis.cli.Experiment compile exec:java
-mv source/synthesis/target/surefire-reports/summary.json source/synthesis/target/surefire-reports/random.json
+cp -r source/synthesis/target/surefire-reports source/synthesis/results-random
 
 mvn --file source/pom.xml --define resynth.synthesis.javadoc=true --define resynth.fuzzing.random=true --define resynth.verification.stage1.maxInputs=$maxInputs --define resynth.verification.stage1.maxCounterexamples=$maxCounterexamples --define resynth.verification.stage2.maxInputs=0 --define resynth.verification.stage2.maxCounterexamples=0 --define resynth.seed=$seed --define failIfNoTests=false --define trimStackTrace=false --define test="uk.ac.ox.cs.refactoring.synthesis.experiment.**" test >random-javadoc.log 2>&1
 mvn --file source/pom.xml --projects='!instrument' --also-make --define exec.classpathScope=test --define exec.mainClass=uk.ac.ox.cs.refactoring.synthesis.cli.Experiment compile exec:java
-mv source/synthesis/target/surefire-reports/summary.json source/synthesis/target/surefire-reports/random-javadoc.json
+cp -r source/synthesis/target/surefire-reports source/synthesis/results-random-javadoc
