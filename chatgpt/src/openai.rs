fbf535cd019bfdf56487130a5cdb380ddccac36b
Hanliang <hanliangzhang.cm@gmail.com>
2023-12-28 12:39:23 -0800

    try new prompt

--- chatgpt/src/openai.rs
+++ chatgpt/src/openai.rs
@@ -64,18 +64,20 @@ pub fn construct_prompt<'a>(
         system("You are an expert in Java."),
         user(format!("I have some Java method calls in my code base but they are deprecated. Refactor them according to \
         the given deprecation comments.\nLet's start with some examples.")),
-        user("The method `java.util.Date.getHours()` is deprecated. \
-        The associated JavaDoc deprecation comment is \"As of JDK version 1.1, replaced by Calendar.get(Calendar.DAY_OF_HOUR).\".\n\
-        Refactor the following code:\n<begin Java code>\nthis.getHours;()\n<end Java code>\nAnswer N/A if there is no solution."),
-        assistant("<begin Java code>\nfinal Calendar calendar = Calendar.getInstance();\n\
-        calendar.setTime(this);\ncalendar.get(Calendar.HOUR_OF_DAY);\n<end Java code>"),
+        user("The method `java.awt.List.addItem(String)` is deprecated. The associated JavaDoc deprecation comment is \n replaced by  add(String) . \n.\nRefactor the following code:\n<begin Java code>\nthis.addItem(param0);\n<end Java code>\nAnswer N/A if there is no solution."),
+        assistant("<begin Java code>\nthis.add(param0);\n<end Java code>"),
+        // user("The method `java.util.Date.getHours()` is deprecated. \
+        // The associated JavaDoc deprecation comment is \"As of JDK version 1.1, replaced by Calendar.get(Calendar.DAY_OF_HOUR).\".\n\
+        // Refactor the following code:\n<begin Java code>\nthis.getHours;()\n<end Java code>\nAnswer N/A if there is no solution."),
+        // assistant("<begin Java code>\nfinal Calendar calendar = Calendar.getInstance();\n\
+        // calendar.setTime(this);\ncalendar.get(Calendar.HOUR_OF_DAY);\n<end Java code>"),
         user("The method `com.sun.jdi.VirtualMachine.canAddMethod()` is deprecated. \
         The associated JavaDoc deprecation comment is \"A JVM TI based JDWP back-end will never set this capability to true.\".\n\
         Refactor the following code:\n<begin Java code>\nthis.canAddMethod();\n<end Java code>\nAnswer N/A if there is no solution."),
         assistant("N/A"),
         user(format!("Now it's time for the real task.\nThe method `{path_name}` is deprecated. \
         The associated JavaDoc deprecation comment is {deprecation_comment}.\nRefactor the following code:\n\
-        <begin Java code>\n{};\n<end Java code>", invoke_method(method_name, args)))
+        <begin Java code>\n{};\n<end Java code>\n\nAnswer N/A if there is no solution.", invoke_method(method_name, args)))
     ]
 }
 
